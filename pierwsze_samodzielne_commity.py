a4e7e5166e62570bf96db22884d4c546b37c7f2c
Radosław Wieczorek <tenkan07@gmail.com>
2023-04-01 13:29:50 +0200

    Init_project

---
+++ pierwsze_samodzielne_commity.py
@@ -0,0 +1,10 @@
+a = "chleb"
+b = "bułki"
+c = "pączki"
+d = "marchew"
+e = "seler"
+f = "rukola"
+lista_zakopow = {
+    "piekarnia" : [a.capitalize(), b.capitalize(), c.capitalize()],
+    "warzywniak" : [d.capitalize(), e.capitalize(), f.capitalize()]
+}
