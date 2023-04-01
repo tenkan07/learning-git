31494dc31756737791bc79b906f0dbe92bff66b0
Radosław Wieczorek <tenkan07@gmail.com>
2023-04-01 13:37:37 +0200

    Init_project1

--- pierwsze_samodzielne_commity.py
+++ pierwsze_samodzielne_commity.py
@@ -8,3 +8,7 @@ lista_zakopow = {
     "piekarnia" : [a.capitalize(), b.capitalize(), c.capitalize()],
     "warzywniak" : [d.capitalize(), e.capitalize(), f.capitalize()]
 }
+
+for i in lista_zakopow:
+    lista_sklepów = lista_zakopow[i]
+    print(f"Idę do " + i.capitalize() + f", kupuję tu następujące rzeczy: {lista_sklepów}")
